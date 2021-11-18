693d151ef9a46ee1644e739dfb9fb2fc48cdfd38
JHAS <jimagui13@hotmail.com>
2021-11-17 23:38:10 -0500

    GATEWAY-SERVER AND CONFIG-CLOUD

--- category/src/main/java/com/nttdata/springboot/category/controller/CategoryController.java
+++ category/src/main/java/com/nttdata/springboot/category/controller/CategoryController.java
@@ -4,6 +4,7 @@ package com.nttdata.springboot.category.controller;
 import com.nttdata.springboot.category.model.Category;
 import com.nttdata.springboot.category.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,18 @@ public class CategoryController {
     @Autowired
     CategoryService categoryService;
 
+    @Value("${category.dato}")
+    private String dato;
+
     @GetMapping("/category")
     public Category getCategory(){
         return categoryService.getCategory();
     }
+
+    @GetMapping("/propertie")
+    public String getPropertie(){
+        return dato;
+    }
+
 
 }
